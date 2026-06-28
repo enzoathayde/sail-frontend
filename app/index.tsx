364bdd60e784efbd0b22fca67e11b488ffb2b4bc
Enzo Athayde <enzo.athayde.dev@gmail.com>
2026-06-27 21:26:55 -0300

    feat: login rule
    - add asyncstorage for jwt

--- app/index.tsx
+++ app/index.tsx
@@ -9,24 +9,36 @@ async function getWelcomeAsyncItem(): Promise<string | null> {
 
   return response;
 }
+async function getJwtAsyncItem(): Promise<string | null> {
+
+  const response = AsyncStorage.getItem('jwt');
+
+  return response;
+}
 
 const Index = () => {
 
   const [hasAlreadyWelcome, setHasAlreadyWelcome] = useState<string | null>(null);
+  const [loggedUser, setLoggedUser] = useState<string | null>(null);
 
   useEffect(() => {
     const getWelcome = async () => {
       const welcomeResult = await getWelcomeAsyncItem()
       setHasAlreadyWelcome(welcomeResult);
     }
+    const getLogged = async () => {
+      const loggedResult = await getJwtAsyncItem()
+      setLoggedUser(loggedResult);
+    }
+    
     getWelcome();
-
+    getLogged();
   },[])
 
   return (
     <>
       { !hasAlreadyWelcome && <Welcome /> }
-      {  hasAlreadyWelcome && <Sign /> }
+      { hasAlreadyWelcome && !loggedUser && <Sign /> }
     </>
   )
 
